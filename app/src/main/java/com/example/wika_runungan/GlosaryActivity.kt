8423fc64fb9cfdaaa22a470dd98eb17438658b87
unknown <bobisjayaustin5@gmail.com>
2023-05-02 15:37:49 +0800

    Second commit

---
+++ app/src/main/java/com/example/wika_runungan/GlosaryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wika_runungan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GlosaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_glosary)
+    }
+}
